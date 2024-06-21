37da435523cdafa475afe1c2f9a7440d5a91a6ec
YazanKanjo <yazan.kanjo@std.medipol.edu.tr>
2024-06-21 16:05:53 +0300

    Update lcd.h

--- lcd.h
+++ lcd.h
@@ -1,10 +1,10 @@
-void delay(unsigned int j);
-void lcd_init_write(unsigned char a);
-void lcd_com(unsigned char a);
-void lcd_data(unsigned char a);
-void lcd_init(void);
-void lcd_puts(char *str);
-void hex2lcd(unsigned char hex);
-void LCD_ScrollMessage(char *msg_ptr);
-void lcd_print_number(unsigned int i);
-void lcd_clear(void);
+void delay(unsigned int j);               // Function to introduce a delay
+void lcd_init_write(unsigned char a);     // Function to send initial commands to the LCD
+void lcd_com(unsigned char a);            // Function to send a command to the LCD
+void lcd_data(unsigned char a);           // Function to send data to the LCD
+void lcd_init(void);                      // Function to initialize the LCD
+void lcd_puts(char *str);                 // Function to print a string on the LCD
+void hex2lcd(unsigned char hex);          // Function to convert a hex value to decimal and display it on the LCD
+void LCD_ScrollMessage(char *msg_ptr);    // Function to scroll a message on the LCD
+void lcd_print_number(unsigned int i);    // Function to print a number on the LCD
+void lcd_clear(void);                     // Function to clear the LCD display
