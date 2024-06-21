19c27c154bd6da9c38eb0bf5dfeed0631792249e
YazanKanjo <yazan.kanjo@std.medipol.edu.tr>
2024-06-21 16:34:50 +0300

    Update tmr0.h

--- tmr0.h
+++ tmr0.h
@@ -1,13 +1,12 @@
-#define INTERRUPT_GlobalInterruptEnable() (INTCONbits.GIE = 1)
-#define INTERRUPT_GlobalInterruptDisable() (INTCONbits.GIE = 0)
-#define INTERRUPT_PeripheralInterruptEnable() (INTCONbits.PEIE = 1)
-#define INTERRUPT_PeripheralInterruptDisable() (INTCONbits.PEIE = 0)
-#define _XTAL_FREQ 200000000 //Crystal Frequency, used in delay
-#define TMR0_INTERRUPT_TICKER_FACTOR    1
-void TMR0_Initialize(void); 
-void TMR0_StartTimer(void); 
-void TMR0_StopTimer(void);
-void TMR0_ISR(void);
-void timer_isr(void);
-void interrupt INTERRUPT_InterruptManager (void);
-
+#define INTERRUPT_GlobalInterruptEnable() (INTCONbits.GIE = 1)       // Enable global interrupts
+#define INTERRUPT_GlobalInterruptDisable() (INTCONbits.GIE = 0)      // Disable global interrupts
+#define INTERRUPT_PeripheralInterruptEnable() (INTCONbits.PEIE = 1)   // Enable peripheral interrupts
+#define INTERRUPT_PeripheralInterruptDisable() (INTCONbits.PEIE = 0)  // Disable peripheral interrupts
+#define _XTAL_FREQ 20000000   // Define crystal frequency for delay calculation
+#define TMR0_INTERRUPT_TICKER_FACTOR    1   // Factor for TMR0 interrupt ticker
+void TMR0_Initialize(void);   // Initialize Timer0
+void TMR0_StartTimer(void);   // Start Timer0
+void TMR0_StopTimer(void);    // Stop Timer0
+void TMR0_ISR(void);          // Timer0 interrupt service routine declaration
+void timer_isr(void);         // Another interrupt service routine declaration
+void interrupt INTERRUPT_InterruptManager(void);  // Handles all interrupts
